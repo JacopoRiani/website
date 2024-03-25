12ae282f71ec26287d43d8441a7ec6c8ee8ea8f0
Jacopo Riani jacopo.riani@gmail.com <jacopo.riani@gmail.com>
2024-03-25 16:09:46 +1100

    homepage and wip projects

--- Gemfile
+++ Gemfile
@@ -35,6 +35,12 @@ gem "redis", ">= 4.0.1"
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
 
+gem "bootstrap", "~> 5.2"
+gem "simple_form"
+gem "sassc-rails"
+gem "dotenv-rails"
+gem "cloudinary"
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ windows jruby ]
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+#Create projects in database
+Project.create!(title: "thanh", details: "Details for Thanh's project")
+Project.create!(title: "dimitri", details: "Details for Dimitri's project")
+Project.create!(title: "germain", details: "Details for Germain's project")
+Project.create!(title: "damien", details: "Details for Damien's project")
+Project.create!(title: "julien", details: "Details for Julien's project")
