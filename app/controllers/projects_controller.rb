66b26d4d6e36eef928c1b7d7a620cc3665e28b53
Jacopo Riani jacopo.riani@gmail.com <jacopo.riani@gmail.com>
2024-04-10 09:34:27 +1000

    project page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,17 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:id])
+    begin
+      @project = Project.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The project you are looking for does not exist."
+      redirect_to projects_path # Redirect to the projects index page or any other appropriate page
+    end
   end
+end
+
+private
+
+def project_params
+  params.require(:project).permit(:title, :details, :image)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root to: "pages#home"
   get "about", to: "pages#about"
-  resources :projects
+  resources :projects, only: [ :index, :show ]
   resources :about
   resources :services
   # Defines the root path route ("/")
