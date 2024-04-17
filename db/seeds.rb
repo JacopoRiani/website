4c313408e92c3ce46e5608d7f4e5a6e26b41f3a1
Jacopo Riani jacopo.riani@gmail.com <jacopo.riani@gmail.com>
2024-04-17 17:41:25 +1000

    static

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,45 +12,45 @@
 
 #Create projects in database
 # Clear existing data
-puts "Cleaning database..."
-Project.destroy_all
+# puts "Cleaning database..."
+# Project.destroy_all
 
-# Create new projects
-venues_suburbs_gap = Project.create!(title: "Venues Suburbs Gap", details: "Analysis of the venues more likely to be chosen by the bookers in the main Australian cities using PowerBi.")
+# # Create new projects
+# venues_suburbs_gap = Project.create!(title: "Venues Suburbs Gap", details: "Analysis of the venues more likely to be chosen by the bookers in the main Australian cities using PowerBi.")
 
-venues_photo = File.open(Rails.root.join("app/assets/images/leadfeed.png"))
-venues_suburbs_gap.image.attach(io: venues_photo, filename: "leadfeed.png", content_type: "image/png")
-venues_suburbs_gap.save!
+# venues_photo = File.open(Rails.root.join("app/assets/images/leadfeed.png"))
+# venues_suburbs_gap.image.attach(io: venues_photo, filename: "leadfeed.png", content_type: "image/png")
+# venues_suburbs_gap.save!
 
-master_thesis = Project.create!(title: "Master Thesis", details: "Analysis and forecast of the motors demand in the Supply Chain. The forecast has been done through Arima model in R.")
+# master_thesis = Project.create!(title: "Master Thesis", details: "Analysis and forecast of the motors demand in the Supply Chain. The forecast has been done through Arima model in R.")
 
-thesis_photo = File.open(Rails.root.join("app/assets/images/thesis.png"))
-master_thesis.image.attach(io: thesis_photo, filename: "thesis.png", content_type: "image/png")
-master_thesis.save!
+# thesis_photo = File.open(Rails.root.join("app/assets/images/thesis.png"))
+# master_thesis.image.attach(io: thesis_photo, filename: "thesis.png", content_type: "image/png")
+# master_thesis.save!
 
-tripquesters = Project.create!(title: "TripQuest", details: "The best way to plan your trips and earn rewards!")
+# tripquesters = Project.create!(title: "TripQuest", details: "The best way to plan your trips and earn rewards!")
 
-trip_photo = File.open(Rails.root.join("app/assets/images/tripque.png"))
-tripquesters.image.attach(io: trip_photo, filename: "tripque.png", content_type: "image/png")
-tripquesters.save!
+# trip_photo = File.open(Rails.root.join("app/assets/images/tripque.png"))
+# tripquesters.image.attach(io: trip_photo, filename: "tripque.png", content_type: "image/png")
+# tripquesters.save!
 
-pokemarkets = Project.create!(title: "Pokemarket", details: "Web application using Ruby, JS, html and css")
+# pokemarkets = Project.create!(title: "Pokemarket", details: "Web application using Ruby, JS, html and css")
 
-poke_photo = File.open(Rails.root.join("app/assets/images/pokemarket.png"))
-pokemarkets.image.attach(io: poke_photo, filename: "pokemarket.png", content_type: "image/png")
-pokemarkets.save!
+# poke_photo = File.open(Rails.root.join("app/assets/images/pokemarket.png"))
+# pokemarkets.image.attach(io: poke_photo, filename: "pokemarket.png", content_type: "image/png")
+# pokemarkets.save!
 
-road_accident = Project.create!(title: "Road Accidents in Italy", details: "Analysis of the road accident in Italy by using available Istat data")
+# road_accident = Project.create!(title: "Road Accidents in Italy", details: "Analysis of the road accident in Italy by using available Istat data")
 
-road_photo = File.open(Rails.root.join("app/assets/images/caraccident.png"))
-road_accident.image.attach(io: road_photo, filename: "caraccident.png", content_type: "image/png")
-road_accident.save!
+# road_photo = File.open(Rails.root.join("app/assets/images/caraccident.png"))
+# road_accident.image.attach(io: road_photo, filename: "caraccident.png", content_type: "image/png")
+# road_accident.save!
 
-levabus = Project.create!(title: "Levabus", details: "Page of a bus fleet using Access and SQL, VBA")
+# levabus = Project.create!(title: "Levabus", details: "Page of a bus fleet using Access and SQL, VBA")
 
-leva_photo = File.open(Rails.root.join("app/assets/images/database.png"))
-levabus.image.attach(io: leva_photo, filename: "database.png", content_type: "image/png")
-levabus.save!
+# leva_photo = File.open(Rails.root.join("app/assets/images/database.png"))
+# levabus.image.attach(io: leva_photo, filename: "database.png", content_type: "image/png")
+# levabus.save!
 
 
 
